706268d07c7da92dec0ab2ad4a1146ba83fcdb72
test <test@test-PC>
2015-08-05 16:39:59 +0530

    Added over loaded method

---
+++ Greetings/src/com/training/Greeting.java
@@ -0,0 +1,16 @@
+package com.training;
+
+public class Greeting {
+	public String getMessage(String name)
+	{
+		return name +"welcome";
+	}
+
+	public String getMessage(){
+		
+		System.out.println("added newly");
+		return"welcome ant with eclipse";
+
+	}
+
+}
